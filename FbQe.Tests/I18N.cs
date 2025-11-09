6caa6a6321801a3c91b5c9c93b86dd1fd64778e3
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2025-11-09 18:59:44 +0100

    Add I18N class and setup unit test project

---
+++ FbQe.Tests/I18N.cs
@@ -0,0 +1,13 @@
+﻿namespace FbQe.Tests
+{
+    [TestClass]
+    public sealed class I18N
+    {
+        [TestMethod]
+        public void GetCurrentUtcTimestamp_IsUtc()
+        {
+            var dt = FbQe.I18N.GetCurrentUtcTimestamp();
+            Assert.AreEqual(DateTimeKind.Utc, dt.Kind);
+        }
+    }
+}
