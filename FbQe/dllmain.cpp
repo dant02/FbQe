f61809b13ed4185330cc10131ea6d4b1cdca944b
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2026-01-11 10:59:31 +0100

    improve arguments names and add list of functions and deployment steps to readme

--- FbQe/dllmain.cpp
+++ FbQe/dllmain.cpp
@@ -4,14 +4,15 @@
 #include "Inc.h"
 
 namespace FbQe {
-    FB_BOOLEAN fUnloadFlag = FB_FALSE;
-    FB_BOOLEAN* fTheirUnloadFlag;
+    FB_BOOLEAN fPluginUnloadFlag = FB_FALSE;
+    FB_BOOLEAN* fEngineUnloadFlag;
 
     std::unique_ptr<Log> l;
     std::unique_ptr<IncFactory> fFactory;
     std::unique_ptr<GetCurrentTimestampUTC_Factory> fGetCurrentTimestampUTC;
 
-    extern "C" __declspec(dllexport) FB_BOOLEAN* __cdecl firebird_udr_plugin(IStatus* status, FB_BOOLEAN* theirUnloadFlag, Firebird::IUdrPlugin* udrPlugin) {
+    extern "C" __declspec(dllexport) FB_BOOLEAN* __cdecl firebird_udr_plugin(IStatus* aStatus, FB_BOOLEAN* aUnloadFlag, Firebird::IUdrPlugin* aPlugin) {
+        fEngineUnloadFlag = aUnloadFlag;
         l = make_unique<Log>();
         l->write_line("Plugin init");
 
@@ -19,12 +20,11 @@ namespace FbQe {
         fGetCurrentTimestampUTC = make_unique<GetCurrentTimestampUTC_Factory>();
 
         l->write_line("Register functions");
-        udrPlugin->registerFunction((ThrowStatusWrapper*)status, "get_int_eks", fFactory.get());
-        udrPlugin->registerFunction((ThrowStatusWrapper*)status, "GetCurrentTimestampUTC", fGetCurrentTimestampUTC.get());
-        fTheirUnloadFlag = theirUnloadFlag;
+        auto status = (ThrowStatusWrapper*)aStatus;
+        aPlugin->registerFunction(status, "get_int_eks", fFactory.get());
+        aPlugin->registerFunction(status, "GetCurrentTimestampUTC", fGetCurrentTimestampUTC.get());
 
         l->write_line("Plugin init end");
-        return &fUnloadFlag;
+        return &fPluginUnloadFlag;
     }
 }
-
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FbQe
-Firebird SQL plugin with goal to ease some tasks
+
+Firebird SQL plugin, https://www.firebirdsql.org
+
+## Contents of the plugin
+
+### Functions
+
+GetCurrentTimestampUTC - returns timestamp of current date and time in UTC
+
+## Deployment steps
+
+- Copy contents of release package to folder \plugins\udr located in the installation folder of Firebird SQL
+- Execute statements in declare_functions.sql
